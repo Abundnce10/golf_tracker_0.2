c33b1f2675efa016d13bb2e0ae6ed621c73fba48
Abundnce10 <michaelstitt@gmail.com>
2013-09-07 12:28:58 -0700

    Working calculateGIR() method in played_rounds.js

--- app/assets/javascripts/played_holes.js
+++ app/assets/javascripts/played_holes.js
@@ -1,3 +1,26 @@
-function calculateGIR() {
-		alert("calculating GIR");
-}
+$(document).ready(function() {
+  $('#played_hole_putts').on('change', function() {
+  	var h1 = $('h1').text();
+    var start = parseInt(h1.indexOf("Par"));
+  	var par = parseInt(h1.substring(start+4,start+5));
+  	var strokes = parseInt($('#played_hole_strokes').val());
+	var putts = parseInt($('#played_hole_putts').val());
+	if ((strokes - putts) <= (par-2)) {
+	  $('#played_hole_GIR').val('1');
+	} else {
+		$('#played_hole_GIR').val('0');
+	}
+  });
+   $('#played_hole_strokes').on('change', function() {
+  	var h1 = $('h1').text();
+    var start = parseInt(h1.indexOf("Par"));
+  	var par = parseInt(h1.substring(start+4,start+5));
+  	var strokes = parseInt($('#played_hole_strokes').val());
+	var putts = parseInt($('#played_hole_putts').val());
+	if ((strokes - putts) <= (par-2)) {
+	  $('#played_hole_GIR').val('1');
+	} else {
+		$('#played_hole_GIR').val('0');
+	}
+  });
+});
