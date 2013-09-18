8dcce78c17011275e5101a0a3b12e234bac8a34e
Abundnce10 <michaelstitt@gmail.com>
2013-09-17 23:17:18 -0700

    Add holeouts and drives to by_par module

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,5 +8,20 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
+  def aggregate_drives(drives_array)
+  	center = 0
+  	left = 0
+  	right = 0
+  	drives_array.each do |i|
+  	  if i == 1
+  	  	center += 1
+  	  elsif i == 2
+  	  	left += 1
+  	  else
+  	  	right += 1
+  	  end
+  	end
+  	return "#{left}/#{center}/#{right}"
+  end
 
 end
